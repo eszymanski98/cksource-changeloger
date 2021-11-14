000e61ee8e18f32a798ba336208e20a99a92d0a8
eszymanski98 <e.szymanski98@gmail.com>
2021-11-14 13:47:21 +0100

    simplified input

--- index.js
+++ index.js
@@ -2,48 +2,29 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 
-const questions = [
-  {
-    type: "input",
-    name: "pathToChangelog",
-    message: "Provide path to the clonned repository of CKEditor 5: ",
-  },
-  {
-    type: "input",
-    name: "previousVer",
-    message: "Provide previous version name: ",
-  },
-  {
-    type: "input",
-    name: "newVer",
-    message: "Provide new version name: ",
-  },
-];
+let pathToRepo = process.argv[2];
+let previousVer = process.argv[3];
+let newVer = process.argv[4];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(JSON.stringify(answers, null, "  "));
+console.log(process.argv);
 
-  const pathToFile = path.join(answers.pathToChangelog, "CHANGELOG.md");
-  const currPath = path.join(__dirname, "CHANGELOG.md");
-
-  // using spawn in the child process module
 let spawn = require('child_process').spawn;
-// start get log process
-git = spawn('git', ['log', '--ancestry-path', '--pretty=format:%s', answers.previousVer + '..' + answers.newVer], {cwd: answers.pathToChangelog}),
-// buffer for data
+
+git = spawn('git', ['log', '--ancestry-path', '--pretty=format:%s', previousVer + '..' + newVer], {cwd: pathToRepo}),
+
 buf = Buffer.alloc(0);
-// concat
+
 git.stdout.on('data', (data) => {
     buf = Buffer.concat([buf, data])
 });
-// if process error
+
 git.stderr.on('data', (data) => {
     console.log(data.toString());
 });
-// when process is done
+
 git.on('close', (code) => {
     fs.writeFile("Changelog.md", buf, function(err){
       if(err) return console.log(err);
     });
 });
-})
+
